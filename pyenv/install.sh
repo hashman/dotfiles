6fed96d01c3eb711ffe33bfe10ae47e9b40d67b3
Hash Lin <coosos810609@gmail.com>
2022-05-22 13:21:04 +0800

    fix(pyenv): return back to current folder

--- pyenv/install.sh
+++ pyenv/install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 BASEDIR=$(dirname "$0")
+CURRENT_DIR=$(pwd)
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-    cd ~/.pyenv && src/configure && make -C src && cd $BASEDIR
+    cd ~/.pyenv && src/configure && make -C src && cd $CURRENT_DIR
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew install pyenv
 fi
