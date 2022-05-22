c558f180ef2a69519170859c459152c9b30cbd0e
Hash Lin <coosos810609@gmail.com>
2022-05-22 08:46:45 +0800

    feat(tmux): install tmux 2.6 for linux os

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 BASEDIR=$(dirname "$0")
-source $BASEDIR/../func.sh
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    runScript 'apt-get -y install tmux'
+    wget https://github.com/tmux/tmux/releases/download/2.6/tmux-2.6.tar.gz
+    tar xvf tmux-2.6.tar.gz cd tmux-2.6
+    ./configure --prefix=$HOME/local && make
+    make install
+    # clean
+    cd .. && rm -rf tmux-2.6.tar.gz tmux-2.6
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew install tmux
 fi
