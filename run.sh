7c3bbacefcb1de3b089dd62dbf4661a3c68e1a5b
Hash Lin <coosos810609@gmail.com>
2022-05-22 16:22:45 +0800

    refactor(run.sh): merge pre-run in run script

--- run.sh
+++ run.sh
@@ -20,6 +20,31 @@ case $package in
             done
         done
         ;;
+    pre-install)
+        if [ -d "$HOME/bin" ]; then
+            info 'bin folder exist, ignore...'
+        else
+            info 'bin folder not exist, create bin folder' && mkdir $HOME/bin
+        fi
+
+        if [ -d "$HOME/local" ]; then
+            info 'local folder exist, ignore...'
+        else
+            info 'local folder not exist, create bin folder' && mkdir $HOME/local
+        fi
+
+        info 'Installing dotfiles for pre-install...'
+        packages=(oh-my-zsh pyenv oh-my-zsh-auto-jump)
+        for pack in "${packages[@]}"; do
+            while true; do
+                read -p "Do you want to install $pack? (y/n) " yn
+                case $yn in
+                    [Yy]* ) info "Install $pack..."; exec_cmd $pack; break;;
+                    * ) break;;
+                esac
+            done
+        done
+        ;;
     *)
         if [ -d "$package" ]; then
             exec_cmd $package
