a7b35b57491363d42ed59c55171963c9a1ecdc89
Hash Lin <coosos810609@gmail.com>
2022-05-22 11:14:42 +0800

    fix(tmux): change binary download folder

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 BASEDIR=$(dirname "$0")
+CURRENT_DIR=$(pwd)
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    wget https://github.com/tmux/tmux/releases/download/2.6/tmux-2.6.tar.gz
-    tar xvf tmux-2.6.tar.gz cd tmux-2.6
+    wget https://github.com/tmux/tmux/releases/download/2.6/tmux-2.6.tar.gz -P /tmp
+    cd /tmp && tar -zxf tmux-2.6.tar.gz && cd tmux-2.6
     ./configure --prefix=$HOME/local && make
     make install
     # clean
-    cd .. && rm -rf tmux-2.6.tar.gz tmux-2.6
+    rm -rf /tmp/tmux-*.tar.gz /tmp/tmux-* && cd $CURRENT_DIR
 fi
 
-cp $BASEDIR/tmux/tmux.conf ~/.tmux.conf
+cp $BASEDIR/tmux.conf ~/.tmux.conf
