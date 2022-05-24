1e5af80ef6359565ea8f7e25b6de970ff2fda2b9
Hash Lin <coosos810609@gmail.com>
2022-05-24 14:39:33 +0800

    feat(git): rename git -> git-config, set vim as global editor

--- git-config/install.sh
+++ git-config/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 cp git/gitignore_global ~/.gitignore_global
+git config --global core.editor "vim"
--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ exec_cmd () { ./$1/install.sh; }
 case $package in
     all)
         info 'Installing dotfiles...'
-        packages=(vim git tig tmux mypager arkade helm awscli tree commitizen openssl-1.1.1g php80 ag)
+        packages=(vim git-config tig tmux mypager arkade helm awscli tree commitizen openssl-1.1.1g php80 ag)
         for pack in "${packages[@]}"; do
             while true; do
                 read -p "Do you want to install $pack? (y/n) " yn
